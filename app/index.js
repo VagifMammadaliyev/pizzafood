e57c69c07de456a4e288cad55152b97fa40fb974
VagifMammadaliyev <vagifmammadaliyev@outlook.com>
2021-04-25 19:20:11 +0400

    error handling when attaching routes

--- app/index.js
+++ app/index.js
@@ -15,7 +15,26 @@ app.init = function () {
   server.init(core.config);
 
   for (const service of services) {
-    server.route(service.route, service.methods, service);
+    let errored = false;
+    if (!Array.isArray(service.methods)) {
+      errored = true;
+      console.log(
+        '\x1b[31m%s\x1b[0m',
+        `Service "${service.name}" has not defined` +
+          ' allowed methods or .method is not an array'
+      );
+    }
+    if (!service.route) {
+      errored = true;
+      console.log(
+        '\x1b[31m%s\x1b[0m',
+        `Service "${service.name}" has not defined routes`
+      );
+    }
+
+    if (!errored) {
+      server.route(service.route, service.methods, service);
+    }
   }
 
   server.serve();
