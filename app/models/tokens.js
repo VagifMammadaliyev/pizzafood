496fdb73150874207f0717f807298b1b07a3941d
VagifMammadaliyev <vagifmammadaliyev@outlook.com>
2021-04-26 22:58:13 +0400

    revoke multiple tokens by their hash

--- app/models/tokens.js
+++ app/models/tokens.js
@@ -17,6 +17,74 @@ tokens.revoke = function (tokenValue) {
   });
 };
 
+tokens.revokeByHash = function (tokenHash) {
+  return new Promise((resolve, reject) => {
+    data.delete('tokens', tokenHash, function (err) {
+      if (!err) {
+        resolve();
+      } else {
+        reject(err);
+      }
+    });
+  });
+};
+
+tokens.revokeMultipleByHash = function (tokenHashes) {
+  return new Promise((resolve, reject) => {
+    // even if there is an error increment the counter.
+    // as if token cannot be read most probably it cannot be accessed
+    // anymore and it can be considered as invalid.
+    let deletedTokens = 0;
+    // but keep track of what happened in case caller want the truth
+    let actuallyDeleted = 0;
+    let notAccessible = 0;
+    let notDeletable = 0;
+    const tokensCount = tokenHashes.length;
+    if (!tokensCount) {
+      resolve({ revoked: 0, notAccessible: 0, actuallyDeleted: 0 });
+    }
+    for (const tokenHash of tokenHashes) {
+      data.read('tokens', tokenHash, function (err) {
+        if (err) {
+          notAccessible++;
+          deletedTokens++;
+        } else {
+          tokens
+            .revokeByHash(tokenHash)
+            .then(() => {
+              actuallyDeleted++;
+              deletedTokens++;
+              return Promise.resolve();
+            })
+            .catch(() => {
+              notDeletable++;
+              deletedTokens++;
+              return Promise.resolve();
+            })
+            .then(() => {
+              if (deletedTokens === tokensCount) {
+                resolve({
+                  revoked: deletedTokens,
+                  notAccessible: notAccessible,
+                  notDeletable: notDeletable,
+                  actuallyDeleted: actuallyDeleted,
+                });
+              }
+            });
+        }
+        if (deletedTokens === tokensCount) {
+          resolve({
+            revoked: deletedTokens,
+            notAccessible: notAccessible,
+            notDeletable: notDeletable,
+            actuallyDeleted: actuallyDeleted,
+          });
+        }
+      });
+    }
+  });
+};
+
 tokens.loadUserFromToken = function (tokenValue) {
   return new Promise((resolve, reject) => {
     const hashedToken = utils.hashString(tokenValue);
